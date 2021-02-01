142c72a54ba56e0be51caebb318aa1bcba8310a2
DESKTOP-20POM8C\Pablo <pablo-pinheiro@hotmail.com>
2021-02-01 10:49:21 -0300

    migrado para a versao 2 da base de dados com o sobrenome

--- app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -2,14 +2,17 @@ package br.com.alura.agenda.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 import br.com.alura.agenda.dao.AlunoDAO;
 import br.com.alura.agenda.model.Aluno;
 
-@Database(entities = {Aluno.class}, version = 1, exportSchema = false)
+@Database(entities = {Aluno.class}, version = 2, exportSchema = false)
 public abstract class AgendaDatabase extends RoomDatabase {
 
     private static final String NOME_BASE_DADOS = "agenda.db";
@@ -19,7 +22,13 @@ public abstract class AgendaDatabase extends RoomDatabase {
     public static AgendaDatabase getInstance(Context context){
       return Room.databaseBuilder(context, AgendaDatabase.class, NOME_BASE_DADOS)
                 .allowMainThreadQueries()
-                .fallbackToDestructiveMigration() //nao pode utilizar isso em producao somente em ambiente de testes devido a destruicao que e realizada no app.
+              .addMigrations(new Migration(1, 2) {
+                  @Override
+                  public void migrate(@NonNull SupportSQLiteDatabase database) {
+                      database.execSQL("ALTER TABLE aluno ADD COLUMN sobrenome TEXT");
+                  }
+              })
+                //.fallbackToDestructiveMigration() //nao pode utilizar isso em producao somente em ambiente de testes devido a destruicao que e realizada no app.
                 .build();
     }
 }
